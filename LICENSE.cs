c67920bf58038c5a5bc951737bda17611a92bc91
Ярослав <shahin_769@mail.ru>
2015-04-18 18:32:18 +0300

    Update LICENSE.cs

--- LICENSE.cs
+++ LICENSE.cs
@@ -12,24 +12,19 @@ namespace ConsoleApplication1
             Console.WriteLine("Введите 2 числа");
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(DVB(a, b));
+            Console.WriteLine("Наименьшее число:" + DVB(a, b));
             Console.ReadLine();
         }
         static int DVB(int a, int b)
         {
             if (a > b)
             {
-                Console.Write(b);
-                Console.WriteLine(" - наименьшее");
                 return b;
             }
-            else if (a < b)
+            else
             {
-                Console.Write(a);
-                Console.WriteLine(" - наименьшее");
                 return a;
             }
-            return a;
         }
     }
 }
